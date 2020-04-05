e4b273f6197eedc75bfae9a07717af8c33196c18
Ilya <moxanatik111@gmail.com>
2020-04-05 16:53:07 +0300

    splitting files into folders

--- ContactsView.swift
+++ ContactsView.swift
@@ -39,9 +39,9 @@ extension ContactsView:UITableViewDataSource, UITableViewDelegate{
 
         return cell!
     }
-//    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-//        return 100
-//    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         print("\(indexPath.row)")
