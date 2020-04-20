1e6d431c663157e4827c2e62c82fd89d2f6956f7
Ilya <moxanatik111@gmail.com>
2020-04-20 11:34:19 +0300

    Stable

--- ViewController.swift
+++ ViewController.swift
@@ -11,11 +11,20 @@ import UIKit
 class ViewController: UIViewController {
 
 
-    
+    @IBAction func buttonClick(_ sender: UIButton) {
+        if sender.titleLabel?.text == "Enter" {
+            let storyboard2 = UIStoryboard(name: "Menu", bundle: nil)
+            let vc = storyboard2.instantiateViewController(identifier: "BarController") as UITabBarController
+            vc.modalPresentationStyle = .fullScreen
+            self.present(vc, animated: true, completion: nil)
+            
+        }
+    }
     
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-       
+
     }
 
 
